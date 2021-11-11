46acb0446dde2248f5295bf04c99e130a3bc0bc9
Israel-Ferreira <israelfsouza@hotmail.com>
2021-11-11 15:57:37 -0300

    Add DatabaseRider

--- crud-quarkus/src/test/java/io/codekaffee/ProdutoTest.java
+++ crud-quarkus/src/test/java/io/codekaffee/ProdutoTest.java
@@ -2,6 +2,9 @@ package io.codekaffee;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import com.github.database.rider.cdi.api.DBRider;
+import com.github.database.rider.core.api.dataset.DataSet;
+
 import org.junit.jupiter.api.Test;
 
 import io.codekaffee.models.Produto;
@@ -10,12 +13,13 @@ import io.quarkus.test.junit.QuarkusTest;
 
 
 
-
+@DBRider
 @QuarkusTest
 @QuarkusTestResource(DatabaseLifecycle.class)
 public class ProdutoTest {
 
     @Test
+    @DataSet("products.yml")
     public void testUm() {
         assertEquals(1, Produto.count());
     }
